3153cdaa396ef82476c65e36eb0b15908bf8b91a
francismeng <francismeng@xwfintech.com>
2019-12-20 17:18:51 +0800

    动态规划

---
+++ dataStructureAndAlgorithm/dynamic_programmer.js
@@ -0,0 +1,31 @@
+function recurFib(n){
+  if(n < 2) {
+    return n
+  }
+  return recurFib(n - 1) + recurFib(n - 2)
+}
+
+function dynFib(n){
+  let val = []
+  for(let i = 0; i < n; i++) {
+    val[i] = 0
+  }
+  if(n === 1 || n ===2) {
+    return 1
+  } else {
+    val[1] = 1
+    val[2] = 1
+    for(let i = 3; i <= n; i++) {
+      val[i] = val[i - 1] + val[i - 2]
+    }
+    return val[n]
+  }
+}
+
+// 递归效率较为低下
+console.time('recur')
+console.log(recurFib(30)) // recur: 12.060ms
+console.timeEnd('recur')
+console.time('dyn')
+console.log(dynFib(30))  // dyn: 0.313ms
+console.timeEnd('dyn') 
